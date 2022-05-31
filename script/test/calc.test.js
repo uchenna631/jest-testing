b5f074f6a32e4f8b94e68a76d185460b5cf0055b
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-05-31 08:24:10 +0000

    Add subtraction, mupltiply and division function

--- script/test/calc.test.js
+++ script/test/calc.test.js
@@ -5,5 +5,22 @@ describe("Calculator", () => {
         test("Should return 42 for 20 + 22", () => {
             expect(addition(20, 22)).toBe(42)
         })
+        
+        test("Should return 73 for 42 + 31", () => {
+            expect(addition(42, 31)).toBe(73)
+        })
+
+    })
+
+    describe("Subtraction function", () => {
+            
+    })
+
+    describe("Multiply function", () => {
+            
+    })
+
+    describe("Division function", () => {
+            
     })
 } )
